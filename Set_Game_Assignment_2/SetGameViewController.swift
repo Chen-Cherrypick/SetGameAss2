473cbed0886d4a6b82d93ca84e0a50f55cef5f9f
Chen Shoresh <chen@cherrypick-consulting.com>
2021-06-21 16:27:37 +0300

    init

---
+++ Set_Game_Assignment_2/SetGameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Set_Game_Assignment_2
+//
+//  Created by Chen Shoresh on 21/06/2021.
+//
+
+import UIKit
+
+class SetGameViewController: UIViewController {
+
+
+
+}
+
